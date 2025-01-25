8a53aa3d86476fe1c77931834bf55ee2b7a5e604
Joe Rogers <frogers@uci.edu>
2025-01-25 10:57:04 -0800

    Added toolbar buttons

--- fast-home/src/MenuToolbar.jsx
+++ fast-home/src/MenuToolbar.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { BsFillMapFill } from "react-icons/bs";
+import { BsBalloonHeart } from "react-icons/bs";
+import { BsFillHouseDoorFill } from "react-icons/bs";
+import { BsChatDots } from "react-icons/bs";
+import { BsFillGearFill } from "react-icons/bs";
 
 const MenuToolbar = () => {
   return (
     <div className="menu-toolbar">
-      <button>Swipe</button>
-      <button>Map</button>
-      <button>Likes</button>
-      <button>Messages</button>
-      <button>Profile</button>
+      <button className="toolbar-btn">
+        <BsFillMapFill size={30} />
+      </button>
+      <button className="toolbar-btn">
+        <BsBalloonHeart size={30} />
+      </button>
+      <button className="toolbar-btn">
+        <BsFillHouseDoorFill size={30} />
+      </button>
+      <button className="toolbar-btn">
+        <BsChatDots size={30} />
+      </button>
+      <button className="toolbar-btn">
+        <BsFillGearFill size={30} />
+      </button>
     </div>
   );
 }
