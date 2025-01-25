8e45f218cb635fce8a948e4abbb124dc13eaf253
kent3245 <your-email@example.com>
2025-01-25 12:08:23 -0800

    testing

--- backend/profile.js
+++ backend/profile.js
@@ -1,16 +1,42 @@
-// Base class: Profile
+const {Pool} = require('pg')
+require('dotenv').config();
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Database connected:', res.rows);
+    }
+    pool.end(); // Close the connection
+});
+
 class Profile {
     constructor(budget, income, occupation, purchase_goal, city) {
-        this.liked_list = []; // Use "this" instead of "self"
+        this.liked_list = []; 
         this.budget = budget;
         this.income = income;
         this.occupation = occupation;
         this.purchase_goal = purchase_goal;
-        this.city = city;
+    }
+    addLikedList(house){
+        this.liked_list.push(house)
     }
 
-    addToDb() {
-        return;
+    async addProfileToDb() {
+        try {
+            const query = `INSERT INTO profile (id,budget,income,occupation,purchse
+                            VALUES ($self)`
+            const res = await pool.query('SELECT * FROM users');
+        } catch (err) {
+            console.error('Error fetching users:', err);
+        }
     }
 }
 
@@ -26,10 +52,6 @@ class Seller extends Profile {
 class Buyer extends Profile {
     constructor(budget, income, occupation, purchase_goal, city) {
         super(budget, income, occupation, purchase_goal, city);
-    }
-
-    addLikedList(house){
-        
     }
 
     matchWithSeller(seller){
