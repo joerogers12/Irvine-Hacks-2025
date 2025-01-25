020582183afd3468bed156fc56a6c43e4d46f8b2
Joe Rogers <frogers@uci.edu>
2025-01-25 10:13:52 -0800

    updated swipe functionality

---
+++ fast-home/src/MenuToolbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MenuToolbar = () => {
+  return (
+    <div className="menu-toolbar">
+      <button>Swipe</button>
+      <button>Map</button>
+      <button>Likes</button>
+      <button>Messages</button>
+      <button>Profile</button>
+    </div>
+  );
+}
+
+export default MenuToolbar;
