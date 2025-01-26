f33f927371c5cb2a95bf883bd3f86ed16424cc0b
Joe Rogers <frogers@uci.edu>
2025-01-25 16:41:46 -0800

    updated the css flexbox

--- SwipeHome/src/components/Data.jsx
+++ SwipeHome/src/components/Data.jsx
@@ -1,34 +1,43 @@
 export default function Data(){
     return(
         <div className="data-container">
-            <div className="AskingValue">
-                <h2>Asking Value</h2>
-                <h3>$800,000</h3>
+            <div className="row1">
+                <div className="AskingValue">
+                    <h2 className="category">Asking Price</h2>
+                    <h3 className="value">$800,000</h3>
+                </div>
             </div>
-            <div className="PredictedValue">
-                <h2>Predicted Value</h2>
-                <h3>$850,000</h3>
+            <div className="row2">
+                <div className="PredictedValue">
+                    <h2 className="category">Predicted Value</h2>
+                    <h3 className="value">$850,000</h3>
+                </div>
             </div>
-            <div className="State">
-                <h4>State</h4>
-                <h4 className="value">CA</h4>
+            <div className="row3">
+                <div className="State">
+                    <h4 className="category">State</h4>
+                    <h4 className="value">CA</h4>
+                </div>
+                <div className="City">
+                    <h4 className="category">City</h4>
+                    <h4 className="value">RSM</h4>
+                </div>
             </div>
-            <div className="City">
-                <h4>City</h4>
-                <h4 className="value">Rancho San Margaritta</h4>
+            <div className="row4">
+                <div className="SQFT">
+                    <h5 className="category">Square footage</h5>
+                    <h5 className="value">858</h5>
+                </div>
+                <div className="YearBuilt">
+                    <h5 className="category">House Built</h5>
+                    <h5 className="value">1997</h5>
+                </div>
+                <div className="Bedrooms">
+                    <h5 className="category">Number of Bedrooms</h5>
+                    <h5 className="value">2</h5>
+                </div>
             </div>
-            <div className="SQFT">
-                <h5>Square footage</h5>
-                <h5 className="value">858</h5>
-            </div>
-            <div className="YearBuilt">
-                <h5>House Built</h5>
-                <h5 className="value">1997</h5>
-            </div>
-            <div className="Bedrooms">
-                <h5>Number of Bedrooms</h5>
-                <h5 className="value">2</h5>
-            </div>
+            
         </div>
     )
 }
